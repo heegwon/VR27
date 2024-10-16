f550eb25a9c3073bbcb6a45bdb425bd0fb55ad2d
heegwon <heegwon0301@gmail.com>
2024-10-16 15:05:13 +0900

    업로드 1회

---
+++ app-files/data.js
@@ -0,0 +1,249 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "파노라마3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07307328572096061,
+        "pitch": 0.21033097173436843,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8790489477559689,
+          "pitch": 0.06944360351888079,
+          "rotation": 0,
+          "target": "5-9"
+        },
+        {
+          "yaw": 0.35999483268712673,
+          "pitch": 0.08036129546596626,
+          "rotation": 0,
+          "target": "1-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7969465775016733,
+          "pitch": -0.1568418135500984,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.8960019886111859,
+          "pitch": 0.08150352009073991,
+          "title": "접근경로 B",
+          "text": ""
+        },
+        {
+          "yaw": 0.4534069013174076,
+          "pitch": 0.07436031677982768,
+          "title": "정면 주출입구",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-5",
+      "name": "파노라마5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.033564024076811805,
+        "pitch": -0.005621992811613907,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09342044374875513,
+          "pitch": 0.07841991275133608,
+          "rotation": 0,
+          "target": "2-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20068337300125982,
+          "pitch": 0.0709433329573752,
+          "title": "실내",
+          "text": ""
+        },
+        {
+          "yaw": 0.033564024076811805,
+          "pitch": -0.005621992811613907,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-6",
+      "name": "파노라마6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7726686752423575,
+        "pitch": 0.049064664537720404,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-7",
+      "name": "파노라마7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-8",
+      "name": "파노라마8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-9",
+      "name": "파노라마9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
